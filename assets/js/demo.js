0b93e683aa9e80ec40ceed692a6b893833e79a0f
Gersón Gómez <ggomez@codemonkey.com.mx>
2020-12-27 13:51:51 -0500

    Commit para cambio de nombre al repositorio

---
+++ assets/js/demo.js
@@ -0,0 +1,84 @@
+
+// const socket = io('ws://localhost:3000');
+// const peer = new Peer(undefined, {
+//   path: '/peerjs',
+//   host: '/',
+//   port: '3030'
+// });
+// const peers = {};
+// const video = document.createElement('video');
+// let streaming;
+//
+// navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: true
+// }).then(stream => {
+//     socket.on('user-connected', userId => {
+//         const call = peer.call(userId, stream);
+//         const video = document.createElement('video');
+//         call.on('stream', userVideoStream => {
+//             addVideoToStreaming(video, userVideoStream);
+//         });
+//         call.on('close', () => {
+//             video.remove();
+//         });
+//         peers[userId] = call;
+//     });
+//     socket.on('createMessage', message => {
+//         $('ul').append('<li class="message"><b>User:</b><br/>' + message + '</li>');
+//         $('.main__chat_window').scrollTop($('.main__chat_window').prop("scrollHeight"));
+//     });
+//     peer.on('call', call => {
+//         call.answer(stream);
+//         const video = document.createElement('video');
+//         call.on('stream', userVideoStream => {
+//             addVideoToStreaming(video, userVideoStream);
+//         });
+//     });
+//     addVideoToStreaming(video, stream);
+//     streaming = stream;
+//     $('html').keydown(function (e) {
+//         if (e.which == 13 && $('input').val().length !== 0) {
+//             socket.emit('message', $('input').val());
+//             $('input').val('');
+//         }
+//     });
+// });
+//
+// socket.on('user-disconnected', userId => {
+//     if (peers[userId]) {
+//         peers[userId].close()
+//     }
+// });
+//
+// peer.on('open', id => {
+//     socket.emit('join-room', ROOM_ID, id);
+// });
+//
+// function addVideoToStreaming(video, stream) {
+//     video.srcObject = stream;
+//     video.addEventListener('loadedmetadata', () => {
+//         video.play();
+//     })
+//     document.getElementById('video-grid').append(video);
+// }
+//
+// const muteUnmute = () => {
+//     if (streaming.getAudioTracks()[0].enabled) {
+//         streaming.getAudioTracks()[0].enabled = false;
+//         document.querySelector('.main__mute_button').innerHTML = '<i class="fas fa-microphone-slash unmute"></i><span>Unmute</span>';
+//     } else {
+//         streaming.getAudioTracks()[0].enabled = true;
+//         document.querySelector('.main__mute_button').innerHTML = '<i class="fas fa-microphone"></i><span>Mute</span>';
+//     }
+// }
+//
+// const playStop = () => {
+//     if (streaming.getVideoTracks()[0].enabled) {
+//         streaming.getVideoTracks()[0].enabled = false;
+//         document.querySelector('.main__video_button').innerHTML = '<i class="stop fas fa-video-slash"></i><span>Play Video</span>';
+//     } else {
+//         streaming.getVideoTracks()[0].enabled = true;
+//         document.querySelector('.main__video_button').innerHTML = '<i class="fas fa-video"></i><span>Stop Video</span>';
+//     }
+// }
--- server.js
+++ server.js
@@ -2,28 +2,46 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, { debug: true });
 const { v4: uuidV4 } = require('uuid');
+// const io = require('socket.io')(server);
+// const { ExpressPeerServer } = require('peer');
+// const peerServer = ExpressPeerServer(server, { debug: true });
 
 app.use(express.static('public'));
-app.use('/peerjs', peerServer);
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => { res.redirect('/' + uuidV4()); });
-app.get('/:room', (req, res) => { res.render('room', { roomId: req.params.room }); });
+app.get('/', (req, res) => { res.render('landingPage'); });
+app.get('/login', (req, res) => { res.render('login'); });
+app.get('/signup', (req, res) => { res.render('signup'); });
+app.get('/profile', (req, res) => { res.render('profile'); });
+app.get('/profile/coins', (req, res) => { res.render('profileCoins'); });
+app.get('/profile/friends', (req, res) => { res.render('profileFriends'); });
+app.get('/profile/record', (req, res) => { res.render('profileRecord'); });
+app.get('/profile/notifications', (req, res) => { res.render('profileNotifications'); });
+app.get('/profile/settings', (req, res) => { res.render('profileSettings'); });
+app.get('/profile/works', (req, res) => { res.render('profileWorks'); });
+app.get('/profile/balance', (req, res) => { res.render('profileBalance'); });
+app.get('/meet', (req, res) => { res.render('meet'); });
+app.get('/meet/random', (req, res) => { res.render('meetRandom'); });
+app.get('/meet/stories', (req, res) => { res.render('meetStories'); });
+app.get('/meet/models', (req, res) => { res.render('meetModels'); });
+app.get('/chats', (req, res) => { res.render('chats'); });
+app.get('/chats/room/write', (req, res) => { res.render('chatsWriteRoom'); });
+app.get('/chats/room/video', (req, res) => { res.render('chatsVideoRoom'); });
+app.get('/demo', (req, res) => { res.redirect('/demo/' + uuidV4()); });
+app.get('/demo/:token', (req, res) => { res.render('demo', { roomId: req.params.room }); });
+// app.use('/peerjs', peerServer);
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-        socket.on('message', (message) => {
-            io.to(roomId).emit('createMessage', message);
-        });
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId);
-        });
-    });
-});
+// io.on('connection', socket => {
+//     socket.on('join-room', (roomId, userId) => {
+//         socket.join(roomId);
+//         socket.to(roomId).broadcast.emit('user-connected', userId);
+//         socket.on('message', (message) => {
+//             io.to(roomId).emit('createMessage', message);
+//         });
+//         socket.on('disconnect', () => {
+//             socket.to(roomId).broadcast.emit('user-disconnected', userId);
+//         });
+//     });
+// });
 
-server.listen(process.env.PORT||3030);
+server.listen(3030);
