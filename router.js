53fd5d5390bedd28492b56d07abdc176aed6e20f
Gersón Gómez <ggomez@codemonkey.com.mx>
2020-12-27 20:55:44 -0500

    Actualización

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/demo', (request, response) => {
-    response.render('demo.html');
+    response.redirect('demo.html');
 });
 
 module.exports = router;
