cb99f5fb8c83a96b7c91cbb07a570d477e96ca22
Gersón Gómez <ggomez@codemonkey.com.mx>
2021-01-02 21:24:12 -0500

    Actualización

--- public/controllers/meet.js
+++ public/controllers/meet.js
@@ -1,13 +1,5 @@
 var video = document.querySelector('#video');
-var canvas = document.querySelector('#canvas');
-var context = canvas.getContext('2d');
-var socket = io();
 
-canvas.style.display = 'none';
-canvas.width = 512;
-canvas.height = 384;
-context.width = canvas.width;
-context.height = canvas.height;
 navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msgGetUserMedia);
 
 navigator.getUserMedia({
@@ -15,19 +7,8 @@ navigator.getUserMedia({
     audio: false
 }, successCamera, errorCamera);
 
-var interval = setInterval(() => {
-    streamCamera(video, context);
-}, 30);
-
-function streamCamera(video, context) {
-    context.drawImage(video, 0, 0, context.width, context.height);
-    socket.emit('stream', canvas.toDataURL('image/webp'));
-}
-
 function successCamera(stream) {
     video.srcObject = stream;
 }
 
-function errorCamera() {
-
-}
+function errorCamera() { }
